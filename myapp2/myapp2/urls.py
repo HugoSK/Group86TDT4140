adec8d8bd2c49bbbc73bdd99b41e75ff5bf5d73a
Ola Lium <olalium@gmail.com>
2017-03-20 13:15:24 +0100

    ok?

---
+++ myapp2/myapp2/urls.py
@@ -0,0 +1,28 @@
+"""myapp2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from login import views
+from django.contrib.auth.views import login
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index_view, name='index'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^login/success/$', views.user_login_success, name='success'),
+    url(r'^register/$', views.register_view, name='register'),
+    url(r'^register/success/$', views.register_success, name='register_success'),
+]
