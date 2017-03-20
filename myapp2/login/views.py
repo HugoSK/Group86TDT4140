af0d2f9211153b64f2469254ab3434bd02f74c6a
Ola Lium <olalium@gmail.com>
2017-03-20 11:16:28 +0100

    Added Django registration

---
+++ myapp2/login/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render_to_response, render
+from login.forms import *
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {'form': form})
+    return render_to_response('register/register.html', variables)
+
+def register_success(request):
+    return render_to_response('register/success.html')
+
+def index_view(request):
+    return render_to_response('index.html')
+
